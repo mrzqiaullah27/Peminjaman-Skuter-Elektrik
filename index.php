9f2956273d164a8041f7091a4edb34747a97dfd4
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-05-22 23:02:06 +0700

    Menambahkan Tampilan Forget Password di HTML

--- index.php
+++ index.php
@@ -1,45 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login & Registration </title>
+  <title>Forgot Password</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
-    <input type="checkbox" id="check">
-    <div class="login form">
-      <header>Login</header>
-      <form action="#">
-        <input type="email" placeholder="Enter your email">
-        <input type="text" placeholder="Enter your username">
-        <input type="password" placeholder="Enter your password">
-        <a href="#">Forgot password?</a>
-        <input type="button" class="button" value="Login">
-      </form>
-      <div class="signup">
-        <span class="signup">Don't have an account?
-         <label for="check">Sign Up</label>
-        </span>
-      </div>
-    </div>
-    <div class="registration form">
-      <header>Sign Up</header>
-      <form action="#">
-        <input type="email" placeholder="Enter your email">
-        <input type="text" placeholder="Create your username">
-        <input type="password" placeholder="Create a password">
-        <input type="password" placeholder="Confirm your password">
-        <input type="button" class="button" value="Sign Up">
-      </form>
-      <div class="signup">
-        <span class="signup">Already have an account?
-         <label for="check">Login</label>
-        </span>
-      </div>
-    </div>
+    <h1>Forgot Password</h1>
+    <form action="/forgot-password" method="post">
+      <label for="email">Enter your email</label>
+      <input type="email" id="email" name="email" placeholder="Email address" required><br><br>
+      <button>Send My Password</button>
+    </form>
   </div>
 </body>
 </html>
