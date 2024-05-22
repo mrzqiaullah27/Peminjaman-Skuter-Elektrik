8a5420abbf359e665f7d0c9bff16f1993ee0f9c6
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-05-22 23:13:45 +0700

    menambahkan kode html jika lupa password

---
+++ forgot.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Forgot Password</title>
+  <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Forgot Password</h1>
+    <form action="/forgot-password" method="post">
+      <label for="email">Enter your email</label>
+      <input type="email" id="email" name="email" placeholder="Email address" required><br><br>
+      <button>Send My Password</button>
+    </form>
+  </div>
+</body>
+</html>
