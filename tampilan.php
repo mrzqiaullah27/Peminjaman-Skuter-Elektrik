a3c0960286d9e325d20bdbca74feeeab34f0acc8
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-05-23 00:16:32 +0700

    Menambahkan kode HTML untuk tampilan awal

---
+++ tampilan.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pilih Peran</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Selamat Datang!</h1>
+        <p>Silakan pilih peran anda:</p>
+        <div class="options">
+            <button class="option" data-role="admin">Admin</button>
+            <button class="option" data-role="pimpinan">Pimpinan</button>
+            <button class="option" data-role="operator">Operator</button>
+        </div>
+    </div>
+</body>
+</html>
