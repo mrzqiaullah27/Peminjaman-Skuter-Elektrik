bafba10754d01dd423a0f7f2083f8664dd20f245
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-05-23 20:29:40 +0700

    Menambahkan kode HTML  untuk konfirmasi password

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!-- Confirmation Message HTML -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Confirmation</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    <h1>Password Confirmation</h1>
+    <p>Thank you, we have sent your password to your email address. Please check your email!</p>
+    <p><a href="link.html">Return to Login Page</a></p>
+</body>
+</html>
