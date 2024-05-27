74b52ea95933053dfbcf18dbade323b63b26b442
aslamsyahid <148352844+aslamsyahid@users.noreply.github.com>
2024-05-27 15:16:21 +0700

    Update view_returns.php

--- view_returns.php
+++ view_returns.php
@@ -38,6 +38,11 @@ if ($result->num_rows > 0) {
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background-color: deepskyblue;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
