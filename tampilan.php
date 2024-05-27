002ac24b4bcb9d6dadafb86fea38ecdccf79e6a4
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-05-27 10:47:06 +0700

    Mengubah tampilan awal

--- tampilan.php
+++ tampilan.php
@@ -7,7 +7,7 @@
 <body>
     <div class="container">
         <h1>Selamat Datang!</h1>
-        <p>Silakan pilih peran anda:</p>
+        <p>Masuk Sebagai:</p>
         <div class="options">
             <button class="option" data-role="admin">Admin</button>
             <button class="option" data-role="pimpinan">Pimpinan</button>
