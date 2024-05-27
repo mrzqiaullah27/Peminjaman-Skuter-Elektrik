cc5746b30746815468b6d22be4b0396881f81079
aslamsyahid <148352844+aslamsyahid@users.noreply.github.com>
2024-05-27 15:14:27 +0700

    Update return_scooter.php

--- return_scooter.php
+++ return_scooter.php
@@ -32,6 +32,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            background-color: deepskyblue;
+        }
+    </style>
 </head>
 <body>
     <div class="container mt-5">
